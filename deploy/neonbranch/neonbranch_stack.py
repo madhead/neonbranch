7403bda2bb1871520cb7e2f56dcb6b3d8a97239a
madhead <siarhei.krukau@gmail.com>
2023-04-12 23:20:34 +0200

    Lambda handler skeleton

--- deploy/neonbranch/neonbranch_stack.py
+++ deploy/neonbranch/neonbranch_stack.py
@@ -1,10 +1,22 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as lmbda
 )
 from constructs import Construct
+
 
 class NeonbranchStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        functionNeonbranch = lmbda.Function(
+            scope=self,
+            id="neonbranch",
+            function_name="neonbranch",
+            architecture=lmbda.Architecture.ARM_64,
+            runtime=lmbda.Runtime.PYTHON_3_9,
+            code=lmbda.Code.from_asset(
+                path="../lambda/neonbranch.zip"
+            ),
+            handler="lambda.neonbranch.handle",
+        )
---
+++ lambda/lambda/neonbranch.py
@@ -0,0 +1,4 @@
+def handle(event, context):
+    return { 
+        'hello' : 'world'
+    }
