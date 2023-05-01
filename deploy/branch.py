0be597a3ab9687c182dfc4afa0c1558ef9506447
madhead <siarhei.krukau@gmail.com>
2023-05-01 21:46:55 +0200

    WIP

--- deploy/branch.py
+++ deploy/branch.py
@@ -27,7 +27,10 @@ def find_branches(project: dict, name: str) -> (dict, dict):
     ).json()['branches']
 
     primary_branch = next(branch for branch in branches if branch['primary'])
-    branch = next((branch for branch in branches if branch['name'] == name), None)
+    if name:
+        branch = next((branch for branch in branches if branch['name'] == name), None)
+    else:
+        branch = primary_branch
 
     return primary_branch, branch
 
@@ -68,16 +71,55 @@ def create_branch(project: dict, parent: dict, name: str) -> dict:
     return result['branch']
 
 
-if __name__ == '__main__':
-    print("NEON_BRANCH:")
-    print(os.environ['NEON_BRANCH'])
-    print(bool(os.environ['NEON_BRANCH']))
+def find_endpoint(project: dict, branch: dict) -> dict:
+    endpoints = requests.get(
+        url=f"{base_url}/projects/{project['id']}/endpoints",
+        headers=headers,
+    ).json()['endpoints']
 
+    return next(endpoint for endpoint in endpoints if endpoint['branch_id'] == branch['id'])
+
+
+def find_role(project: dict, branch: dict) -> dict:
+    roles = requests.get(
+        url=f"{base_url}/projects/{project['id']}/branches/{branch['id']}/roles",
+        headers=headers,
+    ).json()['roles']
+
+    return next(role for role in roles if not role['protected'])
+
+
+def get_password(project: dict, branch: dict, role: dict) -> str:
+    return requests.get(
+        url=f"{base_url}/projects/{project['id']}/branches/{branch['id']}/roles/{role['name']}/reveal_password",
+        headers=headers,
+    ).json()['password']
+
+
+def mask(value: str):
+    print(f"::add-mask::{value}")
+
+
+def set_output(name: str, value: str):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as github_output:
+        print(f'{name}={value}', file=github_output)
+
+
+if __name__ == '__main__':
     project = find_project(os.environ['NEON_PROJECT'])
-    primary_branch, branch = find_branches(project,
-                                           os.environ['NEON_BRANCH'])  # TODO: What id it's main branch? Non PR?
+    primary_branch, branch = find_branches(project, os.environ.get('NEON_BRANCH'))
 
     if not branch:
-        branch = create_branch(project, primary_branch, os.environ['NEON_BRANCH'])
+        branch = create_branch(project, primary_branch, os.environ.get('NEON_BRANCH'))
 
-    print(branch)
+    endpoint = find_endpoint(project, branch)
+    role = find_role(project, branch)
+    password = get_password(project, branch, role)
+
+    mask(endpoint['host'])
+    mask(role['name'])
+    mask(password)
+
+    set_output('db_host', endpoint['host'])
+    set_output('db_user', role['name'])
+    set_output('db_password', password)
